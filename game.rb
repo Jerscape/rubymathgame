63fa59ea478ba1c10b6be55b8a5bd4941987765e
@jerscape <jeremydutton5@gmail.com>
2024-01-21 16:13:42 -0500

    game is running, accepting player names, creating player instances, and basic while loop is loading question and call it's prompt method

--- game.rb
+++ game.rb
@@ -1,8 +1,48 @@
+require_relative './Player'
+require_relative './Question'
+
 class Game 
 
-  
+  attr_accessor :score, :player1, :player2, :player_toggle
 
-  def initialize
+  def initialize()
+
+
+    @player1 = {}
+    @player2 = {}
+
+    puts "Welcome to my Ruby Math Game"
+    puts "----------------------------"
+    puts "This game requires two players"
+    puts " "
+
+    puts "Enter player 1 name"
+    playerName1 = gets.chomp
+    @player1[:instance] = Player.new(playerName1)
+    puts "Welcome, #{@player1[:instance].name}!"
+    puts ""
+    puts "Enter player 2 name"
+    playerName2= gets.chomp
+    @player2[:instance] = Player.new(playerName2)
+    puts "Welcome, #{@player2[:instance].name}!"
+  end
+
+  def play
+
+    #put while loop here
+    #maybe it;s if not while
+    while player1[:instance].lives > 0 && player2[:instance].lives > 0
+      #generate question
+      #use if statement and modulus to toggle
+      question = Question.new()
+
+      puts question.prompt
+      answer = gets.chomp
+
+    end
+    
   end
 
 end
+
+
--- main.rb
+++ main.rb
@@ -1,1 +1,6 @@
-require Player
+require_relative './Game'
+require_relative './Player'
+require_relative './Question'
+
+myGame = Game.new()
+myGame.play
